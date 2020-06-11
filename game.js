b9500633f00489a55473c357be756c5cd3d35927
Brian Han <bmhan319@hotmail.com>
2020-06-10 18:12:11 -0700

    added flipping card functionality

--- game.js
+++ game.js
@@ -1,30 +1,44 @@
 const cards = [
   { name:"cheeseburger", src:"cheeseburger.png", id:1 },
-  { name:"cheeseburger", src:"cheeseburger.png", id:2 },
-  { name:"fries", src:"fries.png", id:3 },
-  { name:"fries", src:"fries.png", id:4 },
-  { name:"hotdog", src:"hotdog.png", id:5 },
-  { name:"hotdog", src:"hotdog.png", id:6 },
-  { name:"ice cream", src:"ice-cream.png", id:7 },
-  { name:"ice cream", src:"ice-cream.png", id:8 },
-  { name:"milkshake", src:"milshake.png", id:9 },
-  { name:"milkshake", src:"milshake.png", id:10 },
-  { name:"pizza", src:"pizza", id:11 },
-  { name:"pizza", src:"pizza", id:12 },
+  { name:"cheeseburger", src:"cheeseburger.png", id:1 },
+  { name:"fries", src:"fries.png", id:2 },
+  { name:"fries", src:"fries.png", id:2 },
+  { name:"hotdog", src:"hotdog.png", id:3 },
+  { name:"hotdog", src:"hotdog.png", id:3 },
+  { name:"ice cream", src:"ice-cream.png", id:4 },
+  { name:"ice cream", src:"ice-cream.png", id:4 },
+  { name:"milkshake", src:"milshake.png", id:5 },
+  { name:"milkshake", src:"milshake.png", id:5 },
+  { name:"pizza", src:"pizza", id:6 },
+  { name:"pizza", src:"pizza", id:6 },
 ]
+
+let cardsFlipped = []
 
 window.addEventListener('load', () => {
   for (let i = 0; i < cards.length; i++) {
     const gameBoard = document.querySelector('.gameBoard')
     let topCard = document.createElement('div')
     topCard.innerHTML = '<img class="topCard" src="/images/blank.png" alt="top of card">'
+    topCard.setAttribute("data-id", cards[i].id)
+    topCard.setAttribute("id", i)
     gameBoard.appendChild(topCard)
-    window.addEventListener('click', flipCard)
+    
   }
 
+  window.addEventListener('click', (e)=> {
+    let el = e.target
+    let parent = el.parentNode
+    let card = parent.getAttribute('data-id')
+    cardsFlipped.push(card)
+    console.log(cardsFlipped)
+    checkForMatch()
+  })
 
-  function flipCard() {
-    console.log("hi")
+  function checkForMatch() {
+    if (cardsFlipped.length === 2) {
+      console.log('hi')
+    }
   }
 
 })
