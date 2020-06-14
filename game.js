1eb1615495e5d09f971894dbeda29ed0520e8d78
Brian Han <bmhan319@hotmail.com>
2020-06-14 15:27:29 -0700

    added feature to set grid size

--- game.js
+++ game.js
@@ -1,27 +1,27 @@
 // Cards in the pile to play Memory with
 const animalCards = [
-  { name:"beaver", src:"animals/beaver.png", id:2 },
-  { name:"beaver", src:"animals/beaver.png", id:2 },
   { name:"cat", src:"animals/cat.png", id:1 },
   { name:"cat", src:"animals/cat.png", id:1 },
+  { name:"beaver", src:"animals/beaver.png", id:2 },
+  { name:"beaver", src:"animals/beaver.png", id:2 },
   { name:"chick", src:"animals/chick.png", id:3 },
   { name:"chick", src:"animals/chick.png", id:3 },
-  { name:"chicken", src:"animals/chicken.png", id:7 },
-  { name:"chicken", src:"animals/chicken.png", id:7 },
-  { name:"cow", src:"animals/cow.png", id:5 },
-  { name:"cow", src:"animals/cow.png", id:5 },
-  { name:"dog", src:"animals/dog.png", id:9 },
-  { name:"dog", src:"animals/dog.png", id:9 },
   { name:"donkey", src:"animals/donkey.png", id:4 },
   { name:"donkey", src:"animals/donkey.png", id:4 },
-  { name:"duck", src:"animals/duck.png", id:11 },
-  { name:"duck", src:"animals/duck.png", id:11 },
+  { name:"cow", src:"animals/cow.png", id:5 },
+  { name:"cow", src:"animals/cow.png", id:5 },
   { name:"elephant", src:"animals/elephant.png", id:6 },
   { name:"elephant", src:"animals/elephant.png", id:6 },
-  { name:"lion", src:"animals/lion.png", id:10 },
-  { name:"lion", src:"animals/lion.png", id:10 },
+  { name:"chicken", src:"animals/chicken.png", id:7 },
+  { name:"chicken", src:"animals/chicken.png", id:7 },
   { name:"monkey", src:"animals/monkey.png", id:8 },
   { name:"monkey", src:"animals/monkey.png", id:8 },
+  { name:"dog", src:"animals/dog.png", id:9 },
+  { name:"dog", src:"animals/dog.png", id:9 },
+  { name:"lion", src:"animals/lion.png", id:10 },
+  { name:"lion", src:"animals/lion.png", id:10 },
+  { name:"duck", src:"animals/duck.png", id:11 },
+  { name:"duck", src:"animals/duck.png", id:11 },
   { name:"penguin", src:"animals/penguin.png", id:12 },
   { name:"penguin", src:"animals/penguin.png", id:12 }
 ]
@@ -59,32 +59,43 @@ const backgrounds = [
 ]
 
 //container variables to store data of each move
-//let cards = foodCards
-let cards = animalCards
-let smallCards = []
-let largeCards = []
+let defaultCards = animalCards  //default selection
+let sizedCards = []             //new array, length based grid size
+let gridSize = 24               //either 12 or 24
+let currentColor = 'green'
+
+
 let numOfMatches = 0
 let cardsFlippedID = []
 let cardFlippedID1 = []
 let cardFlippedID2 = []
 let cardsFlippedIndex = []
 
-//select game board size
-//outputs array that is 12 or 24
-function gameSize(num) {
-  smallCards = []
-  largeCards = []
-  if (num === 24) {
-    cards.forEach( item => {largeCards.push(item)} )
-  } else if (num === 12) {
-    cards.forEach ( item => {
-      if (item.id % 2 === 0) {
-        smallCards.push(item)
-      }
-    })
+//Create GameBoard
+function createBoard(cardSet) {
+  shuffleArray(cardSet)
+  for (let i = 0; i < cardSet.length; i++) {
+    const gameBoard = document.querySelector('.gameBoard')
+    let topCard = document.createElement('div')
+    if (currentColor == 'green') {
+      topCard.innerHTML = '<img class="topCard" src="/images/backCards/blank_green.jpg" alt="top of card">'
+    } else {
+      topCard.innerHTML = '<img class="topCard" src="/images/backCards/blank_red.png" alt="top of card">'
+    }
+    topCard.setAttribute("data-id", cardSet[i].id)
+    topCard.setAttribute("class", "card"+cardSet[i].id)
+    topCard.setAttribute("id", i)
+    gameBoard.appendChild(topCard)
   }
 }
 
+//Delete GameBoard
+function deleteBoard() {
+  const gameBoard = document.querySelector('.gameBoard')
+  while (gameBoard.firstChild) {
+    gameBoard.removeChild(gameBoard.firstChild)
+  }
+}
 
 //shuffle the cards using Fisher-Yates (aka Knuth) Shuffle
 function shuffleArray(array) {
@@ -102,142 +113,145 @@ function shuffleArray(array) {
   return array;
 }
 
-//Create GameBoard
-function createBoard(cardSet) {
-  shuffleArray(cardSet)
-  for (let i = 0; i < cards.length; i++) {
-      const gameBoard = document.querySelector('.gameBoard')
-      let topCard = document.createElement('div')
-      if (cardSet == animalCards) {
-        topCard.innerHTML = '<img class="topCard" src="/images/backCards/blank_green.jpg" alt="top of card">'
-      } else {
-        topCard.innerHTML = '<img class="topCard" src="/images/backCards/blank_red.png" alt="top of card">'
+//select game board size
+//outputs array that is 12 or 24
+function gameSize(num) {
+  sizedCards = []
+  gridSize = num
+  if (num === 24) {
+    defaultCards.forEach( item => {sizedCards.push(item)} )
+  } else if (num === 12) {
+    defaultCards.forEach ( item => {
+      if (item.id < 7 ) {
+        sizedCards.push(item)
       }
-      topCard.setAttribute("data-id", cards[i].id)
-      topCard.setAttribute("class", "card"+cards[i].id)
-      topCard.setAttribute("id", i)
-      gameBoard.appendChild(topCard)
-    }
+    })
+  }
+  deleteBoard()
+  createBoard(sizedCards)
 }
 
-function changeTheme(theme, index) {
+function changeTheme(theme, cardColor, index) {
+  if (theme === defaultCards) {return alert("You are already on that theme")}
   if (confirm('Are you sure?  You will lose all progress in the current game.')) {
     deleteBoard()
-    cards = theme
+    currentColor = cardColor
+    defaultCards = theme
     document.querySelector('.gameContainer').style.backgroundImage = `url('images/backgrounds/${backgrounds[index].bg}')`
-    createBoard()
-  }
-}
-
-//Delete GameBoard
-function deleteBoard() {
-  const gameBoard = document.querySelector('.gameBoard')
-  while (gameBoard.firstChild) {
-    gameBoard.removeChild(gameBoard.firstChild)
+    gameSize(gridSize)
   }
 }
 
 //On Page Load, load gameboard
 window.addEventListener('load', () => {
-  createBoard(cards)
-
-  //On click, start game
-  window.addEventListener('click', (e)=> {
-    let el = e.target
-    let parent = el.parentNode
-    let card = parent.getAttribute('data-id')
-    let cardID = parent.getAttribute('id')
-    let cardItem
-    //When player clicks outside the gameboard, do nothing
-    if (card == null) {
-      return
-    //else flip cards to play the game
-    } else {
-      cardItem = cards[cardID].src
-      cardsFlippedID.push(card)
-      cardsFlippedIndex.push(cardID)
-      el.setAttribute("src", `/images/${cardItem}`)
-      checkForMatch(cardsFlippedIndex)
-    }
-  })
-
-  //after two cards are selected check for match
-  function checkForMatch(index) {
-    if (document.getElementById(index) !== null) {
-      document.getElementById(index).style.pointerEvents = 'none'
-    }
-
-    if (cardsFlippedID.length === 2) {
-      const stopClick = document.querySelector('.gameBoard')
-      let firstSelected = document.getElementById(index[0]).children
-      let secondSelected = document.getElementById(index[1]).children
-      stopClick.style.pointerEvents ="none"
-      cardFlippedID1 = cardsFlippedID[0] 
-      cardFlippedID2 = cardsFlippedID[1]
-
-      //if cards match, do something
-      if (cardFlippedID1 === cardFlippedID2) {
-        const message = document.querySelector('#message')
-        const selectedCards = document.querySelectorAll('.card' + cardFlippedID1)
-        selectedCards.forEach( item => {
-          item.style.pointerEvents = "none"
-        } )
-        
-        message.innerHTML = "Match"
-        numOfMatches++
-        checkForWin()
-        cardsFlippedID = []
-        cardFlippedID1 = []
-        cardFlippedID2 = []
-        cardsFlippedIndex = []
-        stopClick.style.pointerEvents ="auto"
-        
-      
-      // else flip the cards back and reset container variables
-      } else {
-        const message = document.querySelector('#message')
-        message.innerHTML = "Not A Match"
-
-        setTimeout( ()=>{
-          if (cards == animalCards) {
-            firstSelected[0].setAttribute('src', "/images/backCards/blank_green.jpg")
-            secondSelected[0].setAttribute('src', "/images/backCards/blank_green.jpg")
-          } else {
-            firstSelected[0].setAttribute('src', "/images/backCards/blank_red.png")
-            secondSelected[0].setAttribute('src', "/images/backCards/blank_red.png")
-          }
-          stopClick.style.pointerEvents ="auto"
-        }, 500 )
-        document.getElementById(cardsFlippedIndex[0]).style.pointerEvents = 'auto'
-        cardsFlippedID = []
-        cardFlippedID1 = []
-        cardFlippedID2 = []
-        cardsFlippedIndex = []
-      }
-    } 
-  }
-
-  //Check to see if all cards are matched
-  function checkForWin() {
-    if (numOfMatches === (cards.length) / 2) {
-      const message = document.querySelector('#message')
-      confetti.start()
-      message.innerHTML = "You Win!"
-
-       setTimeout( ()=> {
-         if (confirm("Play Again?")) {
-          numOfMatches = 0
-          cardsFlippedID = []
-          cardFlippedID1 = []
-          cardFlippedID2 = []
-          cardsFlippedIndex = []
-          deleteBoard()
-          createBoard()
-          message.innerHTML = ""
-          confetti.stop()
-        }
-       }, 3500 )
-    }
-  }
-
+  createBoard(defaultCards)
 })
+
+
+
+
+
+
+//On click, start game
+window.addEventListener('click', (e)=> {
+  let el = e.target
+  let cards
+  let parent = el.parentNode
+  let card = parent.getAttribute('data-id')
+  let cardID = parent.getAttribute('id')
+  let cardItem
+  //When player clicks outside the gameboard, do nothing
+  if (card == null) {
+    return
+  //else flip cards to play the game
+  } else {
+    if (sizedCards.length === 0) {
+      cards = defaultCards
+    } else {
+      cards = sizedCards
+    }
+    cardItem = cards[cardID].src
+    cardsFlippedID.push(card)
+    cardsFlippedIndex.push(cardID)
+    el.setAttribute("src", `/images/${cardItem}`)
+    checkForMatch(cardsFlippedIndex, cards)
+  }
+})
+
+//after two cards are selected check for match
+function checkForMatch(index, cardSet) {
+  if (document.getElementById(index) !== null) {
+    document.getElementById(index).style.pointerEvents = 'none'
+  }
+
+  if (cardsFlippedID.length === 2) {
+    const stopClick = document.querySelector('.gameBoard')
+    let firstSelected = document.getElementById(index[0]).children
+    let secondSelected = document.getElementById(index[1]).children
+    stopClick.style.pointerEvents ="none"
+    cardFlippedID1 = cardsFlippedID[0] 
+    cardFlippedID2 = cardsFlippedID[1]
+
+    //if cards match, do something
+    if (cardFlippedID1 === cardFlippedID2) {
+      const message = document.querySelector('#message')
+      const selectedCards = document.querySelectorAll('.card' + cardFlippedID1)
+      selectedCards.forEach( item => {
+        item.style.pointerEvents = "none"
+      } )
+      
+      message.innerHTML = "Match"
+      numOfMatches++
+      checkForWin(cardSet)
+      cardsFlippedID = []
+      cardFlippedID1 = []
+      cardFlippedID2 = []
+      cardsFlippedIndex = []
+      stopClick.style.pointerEvents ="auto"
+      
+    
+    // else flip the cards back and reset container variables
+    } else {
+      const message = document.querySelector('#message')
+      message.innerHTML = "Not A Match"
+      setTimeout( ()=>{
+        if (currentColor === 'green') {
+          firstSelected[0].setAttribute('src', "/images/backCards/blank_green.jpg")
+          secondSelected[0].setAttribute('src', "/images/backCards/blank_green.jpg")
+        } else {
+          firstSelected[0].setAttribute('src', "/images/backCards/blank_red.png")
+          secondSelected[0].setAttribute('src', "/images/backCards/blank_red.png")
+        }
+        stopClick.style.pointerEvents ="auto"
+      }, 500 )
+      document.getElementById(cardsFlippedIndex[0]).style.pointerEvents = 'auto'
+      cardsFlippedID = []
+      cardFlippedID1 = []
+      cardFlippedID2 = []
+      cardsFlippedIndex = []
+    }
+  } 
+}
+
+//Check to see if all cards are matched
+function checkForWin(cardSet) {
+  if (numOfMatches === (cardSet.length) / 2) {
+    const message = document.querySelector('#message')
+    confetti.start()
+    message.innerHTML = "You Win!"
+
+      setTimeout( ()=> {
+        if (confirm("Play Again?")) {
+        numOfMatches = 0
+        cardsFlippedID = []
+        cardFlippedID1 = []
+        cardFlippedID2 = []
+        cardsFlippedIndex = []
+        deleteBoard()
+        createBoard(sizedCards)
+        message.innerHTML = ""
+        confetti.stop()
+      }
+      }, 2500 )
+  }
+}
