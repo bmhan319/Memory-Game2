64e9a1c95448f06580296ad0f9351f82ee841f8c
Brian Han <bmhan319@hotmail.com>
2020-06-14 12:59:34 -0700

    addetrying to figure out how to add multiple grid sizes

--- game.js
+++ game.js
@@ -1,59 +1,57 @@
 // Cards in the pile to play Memory with
 const animalCards = [
-  { name:"beaver", src:"animals/beaver.png", id:1 },
-  { name:"beaver", src:"animals/beaver.png", id:1 },
-  { name:"cat", src:"animals/cat.png", id:2 },
-  { name:"cat", src:"animals/cat.png", id:2 },
+  { name:"beaver", src:"animals/beaver.png", id:2 },
+  { name:"beaver", src:"animals/beaver.png", id:2 },
+  { name:"cat", src:"animals/cat.png", id:1 },
+  { name:"cat", src:"animals/cat.png", id:1 },
   { name:"chick", src:"animals/chick.png", id:3 },
   { name:"chick", src:"animals/chick.png", id:3 },
-  { name:"chicken", src:"animals/chicken.png", id:4 },
-  { name:"chicken", src:"animals/chicken.png", id:4 },
+  { name:"chicken", src:"animals/chicken.png", id:7 },
+  { name:"chicken", src:"animals/chicken.png", id:7 },
   { name:"cow", src:"animals/cow.png", id:5 },
   { name:"cow", src:"animals/cow.png", id:5 },
-  { name:"dog", src:"animals/dog.png", id:6 },
-  { name:"dog", src:"animals/dog.png", id:6 },
-  { name:"donkey", src:"animals/donkey.png", id:7 },
-  { name:"donkey", src:"animals/donkey.png", id:7 },
-  { name:"duck", src:"animals/duck.png", id:8 },
-  { name:"duck", src:"animals/duck.png", id:8 },
-  { name:"elephant", src:"animals/elephant.png", id:9 },
-  { name:"elephant", src:"animals/elephant.png", id:9 },
+  { name:"dog", src:"animals/dog.png", id:9 },
+  { name:"dog", src:"animals/dog.png", id:9 },
+  { name:"donkey", src:"animals/donkey.png", id:4 },
+  { name:"donkey", src:"animals/donkey.png", id:4 },
+  { name:"duck", src:"animals/duck.png", id:11 },
+  { name:"duck", src:"animals/duck.png", id:11 },
+  { name:"elephant", src:"animals/elephant.png", id:6 },
+  { name:"elephant", src:"animals/elephant.png", id:6 },
   { name:"lion", src:"animals/lion.png", id:10 },
   { name:"lion", src:"animals/lion.png", id:10 },
-  { name:"monkey", src:"animals/monkey.png", id:11 },
-  { name:"monkey", src:"animals/monkey.png", id:11 },
+  { name:"monkey", src:"animals/monkey.png", id:8 },
+  { name:"monkey", src:"animals/monkey.png", id:8 },
   { name:"penguin", src:"animals/penguin.png", id:12 },
   { name:"penguin", src:"animals/penguin.png", id:12 }
 ]
 
 const dinoCards = [
-  { name:"beaveallosaurus", src:"dinosaurs/allosaurus.png", id:1 },
-  { name:"allosaurus", src:"dinosaurs/allosaurus.png", id:1 },
+  { name:"dimetrodon", src:"dinosaurs/dimetrodon.png", id:12 },
+  { name:"dimetrodon", src:"dinosaurs/dimetrodon.png", id:12 },
   { name:"ankylosaurus", src:"dinosaurs/ankylosaurus.png", id:2 },
   { name:"ankylosaurus", src:"dinosaurs/ankylosaurus.png", id:2 },
   { name:"apatosaurus", src:"dinosaurs/apatosaurus.png", id:3 },
   { name:"apatosaurus", src:"dinosaurs/apatosaurus.png", id:3 },
   { name:"brachiosaurus", src:"dinosaurs/brachiosaurus.png", id:4 },
   { name:"brachiosaurus", src:"dinosaurs/brachiosaurus.png", id:4 },
-  { name:"coelophysis", src:"dinosaurs/coelophysis.png", id:5 },
-  { name:"coelophysis", src:"dinosaurs/coelophysis.png", id:5 },
-  { name:"deinonychus", src:"dinosaurs/deinonychus.png", id:6 },
-  { name:"deinonychus", src:"dinosaurs/deinonychus.png", id:6 },
-  { name:"hypsilophodon", src:"dinosaurs/hypsilophodon.png", id:7 },
-  { name:"hypsilophodon", src:"dinosaurs/hypsilophodon.png", id:7 },
-  { name:"iguanodon", src:"dinosaurs/iguanodon.png", id:8 },
-  { name:"iguanodon", src:"dinosaurs/iguanodon.png", id:8 },
-  { name:"pachycephalosaurus", src:"dinosaurs/pachycephalosaurus.png", id:9 },
-  { name:"pachycephalosaurus", src:"dinosaurs/pachycephalosaurus.png", id:9 },
-  { name:"parasaurolophus", src:"dinosaurs/parasaurolophus.png", id:10 },
-  { name:"parasaurolophus", src:"dinosaurs/parasaurolophus.png", id:10 },
-  { name:"plesiosaurus", src:"dinosaurs/plesiosaurus.png", id:11 },
-  { name:"plesiosaurus", src:"dinosaurs/plesiosaurus.png", id:11 },
-  { name:"pteranodon", src:"dinosaurs/pteranodon.png", id:12 },
-  { name:"pteranodon", src:"dinosaurs/pteranodon.png", id:12 }
+  { name:"hadrosaurus", src:"dinosaurs/hadrosaurus.png", id:5 },
+  { name:"hadrosaurus", src:"dinosaurs/hadrosaurus.png", id:5 },
+  { name:"parasaurolophus", src:"dinosaurs/parasaurolophus.png", id:6 },
+  { name:"parasaurolophus", src:"dinosaurs/parasaurolophus.png", id:6 },
+  { name:"pterodactyl", src:"dinosaurs/pterodactyl.png", id:7 },
+  { name:"pterodactyl", src:"dinosaurs/pterodactyl.png", id:7 },
+  { name:"spinosaurus", src:"dinosaurs/spinosaurus.png", id:8 },
+  { name:"spinosaurus", src:"dinosaurs/spinosaurus.png", id:8 },
+  { name:"stegosaurus", src:"dinosaurs/stegosaurus.png", id:9 },
+  { name:"stegosaurus", src:"dinosaurs/stegosaurus.png", id:9 },
+  { name:"triceratops", src:"dinosaurs/triceratops.png", id:10 },
+  { name:"triceratops", src:"dinosaurs/triceratops.png", id:10 },
+  { name:"tyrannosaurus", src:"dinosaurs/tyrannosaurus.png", id:11 },
+  { name:"tyrannosaurus", src:"dinosaurs/tyrannosaurus.png", id:11 },
+  { name:"volcano", src:"dinosaurs/volcano.png", id:1 },
+  { name:"volcano", src:"dinosaurs/volcano.png", id:1 }
 ]
-
-
 
 const backgrounds = [
   {backOfCard: 'blank_green.jpg', bg:'animalCardsBG.jpg'},
@@ -63,11 +61,30 @@ const backgrounds = [
 //container variables to store data of each move
 //let cards = foodCards
 let cards = animalCards
+let smallCards = []
+let largeCards = []
 let numOfMatches = 0
 let cardsFlippedID = []
 let cardFlippedID1 = []
 let cardFlippedID2 = []
 let cardsFlippedIndex = []
+
+//select game board size
+//outputs array that is 12 or 24
+function gameSize(num) {
+  smallCards = []
+  largeCards = []
+  if (num === 24) {
+    cards.forEach( item => {largeCards.push(item)} )
+  } else if (num === 12) {
+    cards.forEach ( item => {
+      if (item.id % 2 === 0) {
+        smallCards.push(item)
+      }
+    })
+  }
+}
+
 
 //shuffle the cards using Fisher-Yates (aka Knuth) Shuffle
 function shuffleArray(array) {
@@ -86,12 +103,12 @@ function shuffleArray(array) {
 }
 
 //Create GameBoard
-function createBoard() {
-  shuffleArray(cards)
+function createBoard(cardSet) {
+  shuffleArray(cardSet)
   for (let i = 0; i < cards.length; i++) {
       const gameBoard = document.querySelector('.gameBoard')
       let topCard = document.createElement('div')
-      if (cards == animalCards) {
+      if (cardSet == animalCards) {
         topCard.innerHTML = '<img class="topCard" src="/images/backCards/blank_green.jpg" alt="top of card">'
       } else {
         topCard.innerHTML = '<img class="topCard" src="/images/backCards/blank_red.png" alt="top of card">'
@@ -109,7 +126,6 @@ function changeTheme(theme, index) {
     cards = theme
     document.querySelector('.gameContainer').style.backgroundImage = `url('images/backgrounds/${backgrounds[index].bg}')`
     createBoard()
-    //document.querySelector('.topCard').setAttribute('src',`/images/backCards/${backgrounds[index].backOfCard}`)
   }
 }
 
@@ -123,7 +139,7 @@ function deleteBoard() {
 
 //On Page Load, load gameboard
 window.addEventListener('load', () => {
-  createBoard()
+  createBoard(cards)
 
   //On click, start game
   window.addEventListener('click', (e)=> {
@@ -220,7 +236,7 @@ window.addEventListener('load', () => {
           message.innerHTML = ""
           confetti.stop()
         }
-       }, 2500 )
+       }, 3500 )
     }
   }
 
